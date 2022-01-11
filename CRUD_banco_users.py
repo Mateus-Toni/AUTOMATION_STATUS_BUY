bb5da95929a284d2f21ae0efbd074fb2bf9c4cdd
Mateus-Toni <mattytoni04@gmail.com>
2022-01-11 14:28:42 -0300

    finalizado C.R.U.D

--- CRUD_banco_users.py
+++ CRUD_banco_users.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+
 import mysql.connector
 
 NAME = 'root'
@@ -6,23 +6,59 @@ PASSWORD = ''
 NAME_DB = 'asb'
 HOST = 'localhost'
 
+query_create = """insert into usuarios values (default,'{}','{}','{}','{}','{}','{}')"""
+query_read = """select * from usuarios"""
+query_update = """update usuarios set {} = '{}' where {} = '{}'"""
+query_delete = """delete from usuarios where id_user = '{}'"""
+
 
 def open_db(user, password, host, database):
     try:
-        db = mysql.connector.connect(user=user,  password=password, host=host, database=database)
-        cursor = db.cursor()
+        db = mysql.connector.connect(user=user, password=password, host=host, database=database)
+        cursor = db.cursor(dictionary=True)
     except:
         return None, None
     else:
         return db, cursor
 
 
+def create_user(nick_name, nome, sobrenome, email, senha):
+    from datetime import datetime
+    data = datetime.today().date()
+    db, cursor = open_db(NAME, PASSWORD, HOST, NAME_DB)
+    if db:
+        print(query_create.format(nick_name, nome, sobrenome, email, senha, data))
+        cursor.execute(query_create.format(nick_name, nome, sobrenome, email, senha, data))
+        db.commit()
+        db.close()
 
 
-query_create = """insert into usuarios values ('{}','{}','{}','{}','{}','{}')"""
-query_read = """select * from usuários"""
-query_update = """update usuarios set {} = {} where id_user = {}"""
-query_delete = """delete {} from usuarios"""
+def read_users():
+    db, cursor = open_db(NAME, PASSWORD, HOST, NAME_DB)
+    if db:
+        cursor.execute(query_read)
+        users = cursor.fetchall()
+        print(users)
+        for dictionary in users:
+            for key, value in dictionary.items():
+                print(f'{key:-<30}> {value}')
+        db.close()
 
+
+
+def update_user(coluna, valor, modo, valor_modo):
+    db, cursor = open_db(NAME, PASSWORD, HOST, NAME_DB)
+    if db:
+        cursor.execute(query_update.format(coluna,  valor, modo, valor_modo))
+        db.commit()
+        db.close()
+
+
+def delete_user(id_usuario):
+    db, cursor = open_db(NAME, PASSWORD, HOST, NAME_DB)
+    if db:
+        cursor.execute(query_delete.format(id_usuario))
+        db.commit()
+        db.close()
 
 
